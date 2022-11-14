cf2ec765b01c3d1a09e67df34ca9abf71edba1ce
sebas61018 <sebas61018@gmail.com>
2022-11-14 16:28:47 -0500

    segundo avance

---
+++ PROYECTO INTEGRADOR/index.js
@@ -0,0 +1,9 @@
+// elementos login
+
+document.getElementById("button").addEventListener("click",function(){
+    document.querySelector(".popup").style.display = "flex";
+});
+
+document.querySelector(".close").addEventListener("click",function(){
+    document.querySelector(".popup").style.display = "none";
+});
