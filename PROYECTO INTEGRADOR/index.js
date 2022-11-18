80640d8d93ebe70ef1f5265deb3464a26267d5f5
sebas61018 <sebas61018@gmail.com>
2022-11-18 00:20:01 -0500

    arreglos de administrador

--- PROYECTO INTEGRADOR/index.js
+++ PROYECTO INTEGRADOR/index.js
@@ -7,3 +7,4 @@ document.getElementById("button").addEventListener("click",function(){
 document.querySelector(".close").addEventListener("click",function(){
     document.querySelector(".popup").style.display = "none";
 });
+
